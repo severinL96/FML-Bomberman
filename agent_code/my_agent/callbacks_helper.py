127f96e33435278a92f36d9181e2e2c784d0f239
HeavenlySilicone <moritzalkofer@Moritzs-MacBook-Air.local>
2021-03-11 16:47:40 +0100

    proper reshape game state!

--- agent_code/my_agent/callbacks_helper.py
+++ agent_code/my_agent/callbacks_helper.py
@@ -89,12 +89,11 @@ def reshape_game_state(game_state, vector=True):
 
     name,score,bomb,coord = game_state['self']
     info_map[coord]= 1
-    
     for name,score,bomb,coord in game_state['others']:
         info_map[coord]= .8
 
     for coin in game_state['coins']:
-        info_map[coin] = .6
+        info_map[coin] = 0.6
 
     field = game_state['field']
     field = np.where(field == -1, .4, field) #revalue wall
@@ -102,12 +101,11 @@ def reshape_game_state(game_state, vector=True):
     info_map = np.where(field!=0,field,info_map) #add field data to info
 
     explosion = - game_state['explosion_map']/4
-    info_map = np.where(field!=0,field,explosion)
+    info_map = np.where(explosion!=0,explosion,info_map)
 
+    print(info_map)
     for coord, timer in game_state['bombs']:
         info_map[coord] = -(timer/10 + .6)
-
-        
 
     #join all maps
     if vector:
